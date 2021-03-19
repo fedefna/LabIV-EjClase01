79fba140b0731d859d48c57f830bcde10beef794
fedefna <fede.fna@hotmail.com>
2021-03-18 23:46:57 -0300

    Primer Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  public title = 'my-first-project';
+  public edadUno = 0;
+  public edadDos = 0;
+  public promedio: number = 0;
+  public suma: number = 0;
+  public mostrar: boolean = false;
+
+  public calcular(): void{
+    this.promedio = (this.edadUno + this.edadDos)/2;
+    this.suma = this.edadUno + this.edadDos;
+    this.mostrar = true;
+  }
+
+  public limpiar(): void{
+    this.edadUno=0;
+    this.edadDos=0;
+    this.promedio=0;
+    this.suma=0;
+    this.mostrar=false;
+  }
+}
